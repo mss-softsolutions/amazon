d5baa9bcf4a5872df7fd22e9d4c20a8dae4481f3
snehasabbasani <sneha199526@gmail.com>
2021-04-26 21:26:08 +0530

    conflict resolved

--- python.py
+++ python.py
@@ -1,9 +1,11 @@
 
+
 this ias added in master branch
 
 this is added in dev branch
+this is added in dev branch
 
-
+1d6af6a... commited in dev branch
 this is added for git clean
 editing again the same file
 this is python file
